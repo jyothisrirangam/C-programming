f3fabdafff59fb08142e990ae1af992c0d2fc5a9
jyothisrirangam <106659967+jyothisrirangam@users.noreply.github.com>
2022-06-03 12:25:11 +0530

    Add files via upload

---
+++ datatypes.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int a=999; 
+	printf("%i\n",a);
+	int d=43298;
+	printf("%d\n",d);
+	char e='A';
+	printf("%c\n",e);
+	long b=999999999;
+	printf("%ld\n",b);
+	float x=1.222;
+	printf("%f\n",x);
+	double y=102.222;
+	printf("%lf\n",y);
+	int z=101.202;		
+	printf("%i\n",z);
+	
+}
